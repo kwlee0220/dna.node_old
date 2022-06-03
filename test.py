c874276891dab0fd38f90cd6dc00bd6340905232
Kang-Woo Lee <kwlee@etri.re.kr>
2022-06-03 11:06:57 +0900

    .

--- test.py
+++ test.py
@@ -1,21 +1,10 @@
 
-from kafka import KafkaConsumer
+from contextlib import closing
 
-import numpy as np
-import os, json, cv2, random
+from dna.camera.opencv_camera import OpenCvVideFile
 
-from dna.node import TrackEvent
-
-
-consumer = KafkaConsumer('test',
-                        bootstrap_servers=['localhost:9092'],
-                        auto_offset_reset='earliest')
-consumer.subscribe('track_events')
-
-for msg in consumer:
-    key = msg.key.decode('utf-8')
-    value = msg.value.decode('utf-8')
-    ev:TrackEvent = TrackEvent.from_json(value)
-
-
-    print(f'Topic: {msg.topic}, Offset: {msg.offset}, Event: {ev}')
+cam = OpenCvVideFile("data/etri/etri_051.mp4")
+with closing(cam.open()) as cap:
+    while True:
+        frame = cap()
+        print(frame)
