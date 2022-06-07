8d36c1e161051f25cae121f93aa4bfddba45e842
Kang-Woo Lee <kwlee@etri.re.kr>
2022-06-07 09:25:51 +0900

    version 지정 방법 수정

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+import dna
 
 setup( 
     name = 'dna.node',
-    version = '0.0.3',
+    version = dna.__version__,
     description = 'DNA framework',
     author = 'Kang-Woo Lee',
     author_email = 'kwlee@etri.re.kr',
