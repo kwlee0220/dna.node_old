9bec9d2d685438372b68d9b3f3a80f1e1f2689e2
Kang-Woo Lee <kwlee@etri.re.kr>
2022-06-23 09:29:35 +0900

    change handling default detector uri

--- scripts/dna_node_detect.py
+++ scripts/dna_node_detect.py
@@ -7,12 +7,13 @@ import dna
 from dna.camera import Camera, ImageProcessor
 from dna.detect.utils import load_object_detecting_callback
 
+__DEFAULT_DETECTOR_URI = 'dna.detect.yolov5:model=l&score=0.4'
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Detect objects in an video")
     parser.add_argument("conf_path", help="configuration file path")
 
-    parser.add_argument("--detector", help="Object detection algorithm.", default="yolov4")
+    parser.add_argument("--detector", help="Object detection algorithm.", default=__DEFAULT_DETECTOR_URI)
     parser.add_argument("--output", "-o", metavar="csv file", help="output detection file.", default=None)
     parser.add_argument("--output_video", "-v", metavar="mp4 file", help="output video file.", default=None)
     parser.add_argument("--show", "-s", action='store_true')
