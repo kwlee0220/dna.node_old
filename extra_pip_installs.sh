3769b881321344e13aebd877d795702d8b02a1ca
Kang-Woo Lee <kwlee@etri.re.kr>
2022-07-04 17:06:03 +0900

    .

--- extra_pip_installs.sh
+++ extra_pip_installs.sh
@@ -5,3 +5,6 @@ pip install torch torchvision --extra-index-url https://download.pytorch.org/whl
 
 # Detectron2 (Linux Only)
 python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
+
+# CNU SiamMOT dependencies
+pip install gluoncv mxnet imgaug
