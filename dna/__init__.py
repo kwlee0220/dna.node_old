98fd7503ce8d65cbb0e6b76778bb78754b068c19
Kang-Woo Lee <kwlee@etri.re.kr>
2022-12-15 11:36:43 +0900

    update 'version'

--- dna/__init__.py
+++ dna/__init__.py
@@ -4,7 +4,7 @@ from .utils import gdown_file, initialize_logger
 from .pika_execution import PikaConnectionParameters, PikaExecutionServer
 from .conf import *
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 # DEBUG_FRAME_IDX = -1
 # DEBUG_SHOW_IMAGE = False
