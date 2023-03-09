1ac0f2e8a43311a5d695035d5c06d2ffcf0a93b7
Kang-Woo Lee <kwlee@etri.re.kr>
2023-03-09 10:37:26 +0900

    .

--- test_drawer.py
+++ test_drawer.py
@@ -9,41 +9,57 @@ from dna import Box, color, plot_utils
 from dna.camera import Camera, ImageProcessor
 from dna.camera.utils import create_camera_from_conf
 from dna.utils import RectangleDrawer, PolygonDrawer
+from dna.zone import Zone
 
 img = None
 
 camera_conf = OmegaConf.create()
 # camera_conf.uri = "data/2022/crops/etri_07_crop.mp4"
-camera_conf.uri = "data/2021/etri_07.mp4"
+camera_conf.uri = "data/2022/etri_041.mp4"
+# camera_conf.uri = "data/ai_city/ai_city_10.mp4"
 # camera_conf.uri = "data/crossroads/crossroad_04.mp4"
-# camera_conf.uri = "output/result.jpg"
-camera_conf.begin_frame = 314
+# camera_conf.uri = "output/track_07.mp4"
+camera_conf.begin_frame = 100
 camera:Camera = create_camera_from_conf(camera_conf)
 
-coords_list = [
-    # [[646, 399], [800, 460], [1194, 274], [1194, 250], [1421, 251], [1362, 491], [1518, 587],
-    #   [1673, 632], [1683, 1078], [1919, 1076], [1918, 6], [2, 6], [5, 399]],
-    # [[182, 478], [526, 502], [800, 460], [1195, 275], [1195, 208], [181, 201]],
-    # [[71, 490], [6, 492], [7, 441], [87, 440], [93, 472]],
-    #     [1921, 1075], [1920, 2], [0, -2], [-2, 636], [1, 1076], [226, 1077], [174, 826]]
+track_zones = [
+    [[1204, 397], [1183, 412], [1039, 1078], [-1, 1081], [0, 730], [956, 373], [959, 360], [960, 323], [1205, 346]]
 ]
-box_list = [
-    # [0, 420, 320, 740],
-    # [900, 240, 1410, 600]
+blind_zones = [
+    # [0, 850, 1920, 1080],
+]
+exit_zones = [
+    [875, 292, 978, 370],
+    [1177, 323, 1255, 401],
+    [[467, 855], [218, 644], [3, 694], [4, 1076], [451, 1076]]
+]
+zones = [
+    # [[153, 387], [206, 518], [1176, 540]],
+    # [[1326, 894], [1498, 517]],
+    # [[81, 740], [1349, 740]],
+    # [[103, 348], [143, 728]],
+    # [[1562, 517], [1376, 928], [1466, 1049]],
+    # [[338, 548], [1254, 953]],
+    # [[34, 407], [228, 401], [449, 540]],
+    # [[35, 393], [298, 380], [468, 326]],
+    # [[735, 212], [710, 281], [463, 363]],
 ]
 
 with closing(camera.open()) as cap:
     img = cap().image
-# img = cv2.imread("output/result.jpg", cv2.IMREAD_COLOR)
+# img = cv2.imread("conf/ai_city/cam_4.jpg", cv2.IMREAD_COLOR)
 
-for tlbr in box_list:
-    box = Box.from_tlbr(tlbr)
-    img = box.draw(img, color.WHITE, 1)
-for coords in coords_list:
-    img = plot_utils.draw_polygon(img, coords, color.ORANGE, 2)
+for coords in track_zones:
+    img = plot_utils.draw_polygon(img, coords, color.GREEN, 2)
+for coords in exit_zones:
+    img = Zone.from_coords(coords).draw(img, color.ORANGE, line_thickness=2)
+for coords in blind_zones:
+    img = Zone.from_coords(coords).draw(img, color.BLUE, line_thickness=2)
+for coords in zones:
+    img = Zone.from_coords(coords, as_line_string=True).draw(img, color.RED, line_thickness=3)
 
 polygon = []
-# polygon = [[95, 530], [328, 667], [2, 796], [2, 530]]
+# polygon = [[1406, 282], [1768, 294], [1643, 236], [1334, 229]]
 coords = PolygonDrawer(img, polygon).run()
 print(coords)
 
