4b297d53c25ffd229635e030b433a5da6fdce2d3
kwlee <kwlee@etri.re.kr>
2023-05-22 16:58:09 +0900

    .

--- dna/__init__.py
+++ dna/__init__.py
@@ -2,7 +2,7 @@ from .color import BGR, WHITE, RED
 from .types import Box, Size2d, Point, Image, Frame
 from .utils import initialize_logger
 
-__version__ = '2.0.1'
+__version__ = '2.1.0'
 
 DEBUG_FRAME_INDEX = -1
 DEBUG_SHOW_IMAGE = False
