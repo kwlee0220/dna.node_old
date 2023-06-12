f18445bb0133be8b8ac587cc1d31df96d5f863a8
kwlee <kwlee@etri.re.kr>
2023-06-12 09:39:29 +0900

    fix a bug.

--- dna/event/kafka_event_publisher.py
+++ dna/event/kafka_event_publisher.py
@@ -1,25 +1,27 @@
 from __future__ import annotations
+
 from typing import Optional
+from collections.abc import Iterable
 
 from kafka import KafkaProducer
 from omegaconf import OmegaConf
 import logging
 
-from dna.event import KafkaEvent
+from .types import KafkaEvent
 from .event_processor import EventListener
 
 
 class KafkaEventPublisher(EventListener):
-    def __init__(self, kafka_brokers:list[str], topic:str, *, logger:Optional[logging.Logger]=None) -> None:
+    def __init__(self, kafka_brokers:Iterable[str], topic:str, *, logger:Optional[logging.Logger]=None) -> None:
         self.logger = logger
         
-        if kafka_brokers is None or isinstance(kafka_brokers, list):
+        if kafka_brokers is None or not isinstance(kafka_brokers, Iterable):
             raise ValueError(f'invalid kafka_brokers: {kafka_brokers}')
         
         try:
             self.topic = topic
             
-            self.producer = KafkaProducer(bootstrap_servers=kafka_brokers)
+            self.producer = KafkaProducer(bootstrap_servers=list(kafka_brokers))
             if self.logger and self.logger.isEnabledFor(logging.INFO):
                 self.logger.info(f"connect kafka-servers: {kafka_brokers}, topic={self.topic}")
         except BaseException as e:
@@ -35,11 +37,12 @@ class KafkaEventPublisher(EventListener):
     def from_conf(cls, conf:OmegaConf, *, logger:Optional[logging.Logger]=None) -> KafkaEventPublisher:
         return cls(kafka_brokers=conf.kafka_brokers, topic=conf.topic, logger=logger)
 
-    def handle_event(self, ev:KafkaEvent) -> None:
+    def handle_event(self, ev:object) -> None:
         if isinstance(ev, KafkaEvent):
-            key = ev.key()
-            value = ev.serialize()
-            self.producer.send(self.topic, value=value, key=key.encode('utf-8'))
+            self.producer.send(self.topic, value=ev.serialize(), key=ev.key().encode('utf-8'))
+        else:
+            if self.logger and self.logger.isEnabledFor(logging.WARN):
+                self.logger.warn("cannot publish non-Kafka event: {ev}")
 
     def flush(self) -> None:
         self.producer.flush()
